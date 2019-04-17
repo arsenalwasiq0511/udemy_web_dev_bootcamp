64ad183872fe375a61b8840e95b7a1e7b87a30d0
wasiqam <arsenalwasiq0511@gmail.com>
2019-04-17 15:41:43 -0700

    jquery

---
+++ jQuery_selector.js
@@ -0,0 +1,11 @@
+//select all divs and give them a purple background
+$("div").css("background-color", "purple");
+
+//select the divs with class "highlight" and make them 200px wide
+$(".highlight").css("width", "200px");
+
+//select div with id "third" and give it an orange border
+$("#third").css("border", "1px orange solid");
+
+//select the first div only and change its font colour to pink
+$("div").first().css("color", "pink");
